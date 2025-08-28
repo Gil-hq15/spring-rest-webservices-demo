5fe8c03af9e18d5b045751da4bf057e9d3c2615e
gilberto <isidro_hq@hotmail.com>
2025-08-29 04:22:03 +0530

    Product Update Operation Completed

--- src/main/java/com/example/ws/service/ProductService.java
+++ src/main/java/com/example/ws/service/ProductService.java
@@ -30,4 +30,14 @@ public class ProductService {
         products.add(product);
         return product;
     }
+
+    public Product update(Long id, Product product) {
+        return products.stream().filter(p->p.getId().equals(id))
+                .findFirst()
+                .map(existing -> {
+                    existing.setName(product.getName());
+                    existing.setPrice(product.getPrice());
+                    return existing;
+                });
+    }
 }
